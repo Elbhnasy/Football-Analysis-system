38821126817ea09e39047fee65c459c06106909f
Khaled Tarek Elbhnasy <khaledtarek57357@gmail.com>
2024-09-01 11:12:42 +0300

    make sure video utils module work

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from utils.videos_utils import read_video, save_video
+
 def main():
-    print("Hello, world!")
+    input_video_path = "/home/fox/Desktop/Football-Analysis-system/videos/input_videos/08fd33_4.mp4"
+    output_video_path = "/home/fox/Desktop/Football-Analysis-system/videos/output_videos/output.avi"
+
+    input_video_frames = read_video(input_video_path)
+    save_video(input_video_frames, output_video_path)
 
 if __name__ == "__main__":
     main()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,1 @@
-from video_utils import read_video, save_video
+from .videos_utils import read_video, save_video
