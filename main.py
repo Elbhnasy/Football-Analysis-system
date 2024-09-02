71445f1a55e9669a82319abf7472d823c7f4e619
Khaled Tarek Elbhnasy <khaledtarek57357@gmail.com>
2024-09-03 02:52:21 +0300

    feat: Add PlayerBallAssigner class for assigning ball to players

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from utils.videos_utils import read_video, save_video
 from team_assigner.team_assigner import TeamAssigner
 from trackers.tracker import Tracker
+from player_ball_assigner.player_ball_assigner import PlayerBallAssigner  
 
 def main():
     input_video_path = "/home/fox/Desktop/Football-Analysis-system/videos/input_videos/08fd33_4.mp4"
@@ -36,13 +37,28 @@ def main():
                                                 player_id)
             tracks['players'][frame_num][player_id]['team'] = team 
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    # Assign Ball Aquisition
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_players(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control= np.array(team_ball_control)
 
     # Draw output 
     ## Draw object Tracks
-    output_video_frames = tracker.draw_annotations(input_video_frames, tracks,team_ball_control)
+    output_video_frames = tracker.draw_annotations(input_video_frames, tracks, team_ball_control)
+
 
     # Save the output video
     save_video(output_video_frames, output_video_path)
+    
 if __name__ == "__main__":
     main()
 
