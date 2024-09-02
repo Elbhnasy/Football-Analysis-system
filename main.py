6775996a91bc654ce441067fc01b40423f8bd512
Khaled Tarek Elbhnasy <khaledtarek57357@gmail.com>
2024-09-03 01:12:39 +0300

    feat: Add lazy loading for images and team assignment functionality
    The code changes include the addition of lazy loading for images to improve page load performance. Additionally, the `TeamAssigner` class has been added to assign teams to players. This commit enhances the functionality of the code by adding lazy loading for images and team assignment functionality.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import cv2
+import numpy as np
 from utils.videos_utils import read_video, save_video
+from team_assigner.team_assigner import TeamAssigner
 from trackers.tracker import Tracker
 
 def main():
@@ -17,6 +20,29 @@ def main():
     tracks = tracker.get_object_tracks(input_video_frames,
                                     read_from_stub=True,
                                     stub_path=stub_path)
+    # Interpolate Ball Positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_colors(input_video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(input_video_frames[frame_num],   
+                                                track['bbox'],
+                                                player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    # Draw output 
+    ## Draw object Tracks
+    output_video_frames = tracker.draw_annotations(input_video_frames, tracks,team_ball_control)
+
+    # Save the output video
+    save_video(output_video_frames, output_video_path)
 if __name__ == "__main__":
     main()
 
